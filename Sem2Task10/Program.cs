cce416605e75d9640474752ad726c6f3b4075d82
vidok <allu89@yandex.ru>
2022-12-23 18:28:48 +0300

    homework2

---
+++ Sem2Task10/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите трёхзначное число: ");
+
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+
+string stringNumber = Convert.ToString(threeDigitNumber);
+
+Console.WriteLine("вторая цифра этого числа -> "+stringNumber[1]);
+                          
---
+++ Sem2Task13/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+string NumberText = Convert.ToString(Number);
+if (NumberText.Length > 2){
+  Console.WriteLine("третья цифра -> " + NumberText[2]);
+}
+else {
+  Console.WriteLine("-> третьей цифры нет");
+}
