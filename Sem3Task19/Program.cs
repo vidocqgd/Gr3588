c19b8ce80a3975580e3ccf562d4e5f374bc0251c
vidok <allu89@yandex.ru>
2022-12-27 16:30:07 +0300

    Homework 3

---
+++ Sem3Task19/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число: ");
+string? number = Console.ReadLine();
+
+void CheckingNumber(string number){
+  if (number[0]==number[4] || number[1]==number[3]){
+    Console.WriteLine($"Число: {number} - является палиндромом.");
+  }
+  else Console.WriteLine($"Число: {number} - не является палиндромом.");
+}
+
+if (number!.Length == 5){
+  CheckingNumber(number);
+}
+else Console.WriteLine($"Введите верное число");
---
+++ Sem3Task23/Program.cs
@@ -0,0 +1,27 @@
+﻿int number = ReadData("Введите число: ");
+
+string outLine = LineBilder(number, 3);
+Printdata("", outLine);
+
+
+void Printdata(string msg, string res)
+{
+    Console.WriteLine(msg + res);
+}
+
+string LineBilder(int n, int pow)
+{
+    string line = string.Empty;
+    for (int i=1; i<n; i++)
+    {
+            line = line + Math.Pow (i, pow) + " ";
+    }
+    line = line + Math.Pow (n, pow);
+    return line;
+}
+
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
