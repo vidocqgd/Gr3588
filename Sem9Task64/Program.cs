da9d55cb71f594912ae0f8db3322df7137fc9f46
vidok <allu89@yandex.ru>
2023-01-15 16:16:55 +0300

    Homework9

---
+++ Sem9Task64/Program.cs
@@ -0,0 +1,22 @@
+﻿int n = InputInt("Введите положительное число ");
+int m = 1;
+if (n < 1)
+{
+    Console.WriteLine("Вы ввели неположительное число");
+}
+Console.WriteLine(NaturalNumber(n, m));
+
+int NaturalNumber(int n, int m)
+{
+    if (n == m)
+        return n;
+    else
+        Console.Write($"{NaturalNumber(n, m + 1)}, ");
+    return m;
+}
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
